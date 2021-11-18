35a07d64a2753bfa7792c47e317579dc1733f128
Mitesh Jadav <jadavmitesh.jd@gmail.com>
2021-11-18 12:29:39 +0530

    Promises

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function delay(seconds, callback) {
-  setTimeout(callback, seconds * 1000);
-}
-
-console.log('Starting delays');
-
-delay(2, () => {
-  console.log('two seconds');
-  delay(1, () => {
-    console.log('Three seconds');
-    delay(1, () => {
-      console.log('four seconds');
-    });
+var delay = (seconds) =>
+  new Promise((resolves, rejects) => {
+    setTimeout(() => {
+      resolves('The long delay has ended');
+    }, seconds * 1000);
   });
-});
+
+delay(1)
+  .then(console.log)
+  .then(() => 43)
+  .then((number) => console.log(`Hello World: ${number}`));
+
+console.log('end first tick');
