be9dd6ed2158ca2ed704c65dfc75a5462c7bcb98
Mitesh Jadav <jadavmitesh.jd@gmail.com>
2021-11-18 12:37:19 +0530

    unhandled promise error handling using catch

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var delay = (seconds) =>
   new Promise((resolves, rejects) => {
+    if (seconds > 3) {
+      rejects(new Error(`${seconds} is too long ! `));
+    }
+
     setTimeout(() => {
       resolves('The long delay has ended');
     }, seconds * 1000);
   });
 
-delay(1)
+delay(4)
   .then(console.log)
   .then(() => 43)
-  .then((number) => console.log(`Hello World: ${number}`));
+  .then((number) => console.log(`Hello World: ${number}`))
+  .catch((error) => console.log(`error : ${error.message}`));
 
 console.log('end first tick');
